f2dc691bb3573e87d0cd8677e3009b11c43cb4fd
Grayson Spencer <graysonspencer1212@gmail.com>
2021-11-24 20:08:35 -0700

    Added some state enums and the clcikable label.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "clickablelabel.h"
+#include <QObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    clickableLabel test;
+    QObject* obj = &test;
+    obj->setProperty("ingredientType", "bourbon");
 }
 
 MainWindow::~MainWindow()
