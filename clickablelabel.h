dd5ba4d4acfe5640fee17945e3985efec4fd353a
David McLean <u0145629@eng.utah.edu>
2021-11-29 17:51:05 -0700

    Added resources file and test images for the bar and a bottle

--- clickablelabel.h
+++ clickablelabel.h
@@ -6,21 +6,15 @@
 #include <QPoint>
 #include <QMouseEvent>
 #include <Qt>
-#include "stateEnums.h"
 
 //Class that allows for registering mouse click events inside a label
 class clickableLabel : public QLabel {
-    Q_OBJECT
-    Q_PROPERTY(Ingredients ingredientType READ getIngredient WRITE setIngredient DESIGNABLE true);
-
+Q_OBJECT
 public:
     explicit clickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
     ~clickableLabel();
-    void setIngredient(Ingredients);
-    Ingredients getIngredient() const;
 
 private:
-    Ingredients thisIngredient;
 
 signals:
     void clicked(const QPoint&);
